c8b62198682fcb3b286fd7f748e008588de98eeb
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-09-25 03:35:08 +0200

    implement entire new short link POST request handler

--- server.js
+++ server.js
@@ -1,37 +1,49 @@
-'use strict';
+const express = require('express');
+const db = require('./db');
+const cors = require('cors');
+const url = require('url');
+const dns = require('dns');
+const bodyParser = require('body-parser');
+const shortid = require('shortid');
+const app = express();
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-
-var cors = require('cors');
-
-var app = express();
-
-// Basic Configuration 
-var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-
+app.use(cors({ optionSuccessStatus: 200 }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.post('/api/shorturl/new', function (req, res) {
+  const parsedUrl = url.parse(req.body.url);
+
+  if (parsedUrl.hostname && (parsedUrl.protocol === "https:" || parsedUrl.protocol === "http:")) {
+    dns.lookup(parsedUrl.hostname, 4, function (err) {
+      if (err) return res.json({ error: "invalid URL" })
+      db.getDb().collection('urls').findOne({ original_url: req.body.url }, (function (err, result) {
+        if (err) throw err;
+        if (!result) return addNew();
+        else res.json({ original_url: result.original_url, short_url: result._id });
+      })
+      );
+    })
+  }
+  else res.json({ error: "invalid URL" })
+
+  function addNew() {
+    db.getDb().collection('urls').insertOne({ _id: shortid.generate(), original_url: req.body.url }, function (err, results) {
+      if (err) throw err;
+      res.json({ "original_url": req.body.url, "short_url": results.insertedId });
+    })
+  }
+})
 
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+app.listen(process.env.PORT, function () {
+  console.log(`Node.js listening on port ${process.env.PORT}`);
 });
