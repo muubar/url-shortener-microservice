c034f7c7bc39c9599dc3a01ea49d9fff6bc41fa0
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-09-26 16:57:24 +0200

    set db name to an environment variable

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var _db;
 MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, function (err, db) {
   if (err) throw err;
   console.log("Successfully connected to MongoDB.");
-  _db = db.db('url')
+  _db = db.db(process.env.MONGODB_DB)
 })
 
 function getDb() { return _db; }
